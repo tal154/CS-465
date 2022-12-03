2c3b636e6e7ca46d62e527a972f8ddabe47607f6
tal154 <tonylee154@gmail.com>
2022-12-02 23:21:37 -0800

    Add Trip

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { trips } from '../data/trips';
+import { Trip } from '../models/trip';
+import { TripDataService } from '../services/trip-data.service';
 
 @Component({
   selector: 'app-trip-listing',
   templateUrl: './trip-listing.component.html',
-  styleUrls: ['./trip-listing.component.css']
+  styleUrls: ['./trip-listing.component.css'],
+  providers: [TripDataService]
 })
 export class TripListingComponent implements OnInit {
 
-  trips: Array<any> = trips;
+  trips: Trip[];
 
-  constructor() { }
+  message: string;
 
-  ngOnInit() {
+  constructor(private tripDataService: TripDataService) { }
+
+  private getTrips(): void {
+    console.log('Inside TripListingComponent#getTrips');
+    this.message = 'Searching for trips';
+    this.tripDataService
+      .getTrip()
+        .then(foundTrips => {
+          this.message = foundTrips.length > 0 ? '' : 'No trips found';
+          this.trips = foundTrips;
+        });
   }
 
+  ngOnInit(): void {
+    this.getTrips();
+  }
 }
